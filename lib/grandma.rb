4981e81a2cb1fcd779ac9e12efa51cd34bbac5aa
Mary Jiang <mesuper222@gmail.com>
2017-08-09 13:10:51 +0000

    finished lab

--- lib/grandma.rb
+++ lib/grandma.rb
@@ -1,2 +1,22 @@
 # Write your code here!
-
+def talk_to_grandma(speak)
+   if speak != speak.upcase
+       return "HUH?! SPEAK UP, SONNY!"
+   else
+       date = rand(1930..1950)
+       return "NO, NOT SINCE #{date}!"
+   end
+end 
+count = 1
+grandma = true
+while grandma == true
+    puts "What do you want to say to grandma?"
+    speak = gets.chomp
+    if speak == "BYE" && count == 3
+        grandma = false
+    elsif speak == "BYE" && count != 3
+        count = count + 1
+    else
+        puts talk_to_grandma(speak)
+    end
+end
